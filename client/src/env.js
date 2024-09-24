8c979e98289939b779c53a30e6f1d14dc595b836
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-24 15:06:47 -0600

    Getting the environment variabes set up

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-yptxrrc8jmvr0wmz.us.auth0.com'
+export const clientId = 'wT1PhGZYQz85SXv9SA4TnybT4ov26x6B'
+export const audience = 'https://auth.speedwagonfoundation.com'
