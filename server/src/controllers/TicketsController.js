b2d6f77b17f74689ae4dd9ebe504aff5a3d0f283
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-25 17:55:18 -0600

    Deleting ticket - postman

--- server/src/controllers/TicketsController.js
+++ server/src/controllers/TicketsController.js
@@ -8,6 +8,7 @@ export class TicketsController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createTicket)
+      .delete('/:ticketId', this.deleteTicket)
   }
 
   async createTicket(request, response, next) {
@@ -21,4 +22,16 @@ export class TicketsController extends BaseController {
       next(error)
     }
   }
+
+  async deleteTicket(request, response, next) {
+    try {
+      const ticketId = request.params.ticketId
+      const userId = request.userInfo.id
+      const message = await ticketsService.deleteTicket(ticketId, userId)
+      response.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
+
 }
--- server/src/services/TicketsService.js
+++ server/src/services/TicketsService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 
 class TicketsService {
@@ -17,6 +18,14 @@ class TicketsService {
   async getTicketsByEvent(eventId) {
     const tickets = await dbContext.Tickets.find({ eventId: eventId }).populate('profile')
     return tickets
+  }
+
+  async deleteTicket(ticketId, userId) {
+    const ticketToDelete = await dbContext.Tickets.findById(ticketId)
+    if (!ticketToDelete) throw Error(`Could not delete, no ticket with id: ${ticketId}`)
+    if (userId != ticketToDelete.accountId) throw new Forbidden(`Not your ticket to delete`)
+    await ticketToDelete.deleteOne()
+    return 'You gave up your ticket to this event'
   }
 
 
