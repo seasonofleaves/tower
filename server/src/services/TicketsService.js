f7359d952029c1c533b207eef4d951060a94096c
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-25 17:19:25 -0600

    Get my tickets - postman

--- server/src/services/TicketsService.js
+++ server/src/services/TicketsService.js
@@ -1,13 +1,19 @@
 import { dbContext } from "../db/DbContext.js"
 
 
-class TicketsService{
-async createTicket(ticketData){
-  const ticket = await dbContext.Tickets.create(ticketData)
-  await ticket.populate('event')
-  await ticket.populate('profile')
-  return ticket
-}
+class TicketsService {
+  async createTicket(ticketData) {
+    const ticket = await dbContext.Tickets.create(ticketData)
+    await ticket.populate('event')
+    await ticket.populate('profile')
+    return ticket
+  }
+
+  async getMyTickets(userId){
+    const tickets = await dbContext.Tickets.find({accountId: userId}).populate('event')
+    return tickets
+  }
+
 }
 
 export const ticketsService = new TicketsService()
