199320324bff11a814effee32e84ed50b923e9ac
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-25 13:59:55 -0600

    Created modal wrapper and added it to home page

---
+++ client/src/components/ModalWrapper.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+defineProps({modalId: String})
+
+</script>
+
+
+<template>
+<div class="modal fade" :id="modalId">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-body">
+        <slot>
+        </slot>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import EventForm from '@/components/EventForm.vue';
 import EventCard from '@/components/globals/EventCard.vue';
+import ModalWrapper from '@/components/ModalWrapper.vue';
 import { eventsService } from '@/services/EventsService.js';
 import { logger } from '@/utils/Logger.js';
 import Pop from '@/utils/Pop.js';
@@ -25,9 +27,19 @@ async function getAllEvents(){
 
 <template>
  <div class="container">
+  <ModalWrapper id="event-form">
+    <EventForm/>
+  </ModalWrapper>
   <section class="row">
     <div class="col-12">
       <h1>Upcoming Events</h1>
+    </div>
+  </section>
+  <section class="row">
+    <div class="col-6 col-md-3">
+      <button data-bs-toggle="modal" data-bs-target="#event-form" class="btn bg-info p-0 border-0 w-100">
+        <div>Create Event</div>
+      </button>
     </div>
   </section>
   <section class="row">
