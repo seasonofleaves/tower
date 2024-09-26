653d3ab9241c2e903cae61e84e69fb3cc4a43fae
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-26 13:46:42 -0600

    Added comment schema to dbContext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { EventSchema } from '../models/Event.js';
 import { TicketSchema } from '../models/Ticket.js';
+import { CommentSchema } from '../models/Comment.js';
 
 
 class DbContext {
-
+  Comments = mongoose.model('Comment', CommentSchema)
   Tickets = mongoose.model('Ticket', TicketSchema)
   Events = mongoose.model('TowerEvent', EventSchema);
   Account = mongoose.model('Account', AccountSchema);
